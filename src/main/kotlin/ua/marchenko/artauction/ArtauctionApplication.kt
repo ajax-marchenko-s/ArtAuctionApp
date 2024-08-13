47a82abff3d8b5b63ac84d54e5f445855ae64811
Svitlana Marchenko <svitlana@Svitlanas-MacBook-Pro.local>
2024-08-13 14:58:00 +0300

    first commit

---
+++ src/main/kotlin/ua/marchenko/artauction/ArtauctionApplication.kt
@@ -0,0 +1,11 @@
+package ua.marchenko.artauction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtauctionApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtauctionApplication>(*args)
+}
