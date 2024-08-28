50cb207171288e692f21a1d1c7bf45fded1d0e27
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-08-28 16:46:17 +0300

    feat: add test controller

---
+++ src/main/kotlin/ua/marchenko/artauction/common/annotation/TestController.kt
@@ -0,0 +1,35 @@
+package ua.marchenko.artauction.common.annotation
+
+import java.util.concurrent.TimeUnit
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ua.marchenko.artauction.common.annotation.profiling.annotation.CustomProfiling
+import ua.marchenko.artauction.common.annotation.scheduled.annotation.CustomScheduled
+import ua.marchenko.artauction.common.annotation.scheduled.scheduledDetails.enums.Day
+
+//NOTE: this is controller to test annotation due to methods from bl are not implemented yet
+@RestController
+@RequestMapping("/api/v1/test")
+class TestController(val testService: TestService) {
+
+    @GetMapping
+    fun test(): String {
+        return testService.test()
+    }
+}
+
+interface TestService {
+    fun test(): String
+}
+
+@Service
+class TestServiceImpl : TestService {
+    @CustomScheduled(day = Day.ALL, hours = 16, minutes = 43)
+    @CustomProfiling(timeUnit = TimeUnit.MICROSECONDS)
+    override fun test(): String {
+        println("TestService")
+        return "TestService"
+    }
+}
