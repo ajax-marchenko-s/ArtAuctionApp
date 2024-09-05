078dd8180831808f9068466524ded6127c0a89b9
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-09-05 14:23:07 +0300

    feat: add delta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.github.surpsg.deltacoverage.gradle.DeltaCoverageConfiguration
+
 plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
@@ -6,6 +8,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
     `java-test-fixtures`
     jacoco
+    id("io.github.surpsg.delta-coverage") version "2.4.0"
 }
 
 group = "ua.marchenko"
@@ -48,4 +51,16 @@ tasks.withType<Test> {
     useJUnitPlatform()
     testLogging { showStandardStreams = true }
     systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
+}
+
+configure<DeltaCoverageConfiguration> {
+    val targetBranch = project.properties["diffBase"]?.toString() ?: "refs/remotes/origin/master"
+    diffSource.byGit {
+        compareWith(targetBranch)
+    }
+
+    violationRules.failIfCoverageLessThan(0.6)
+    reports {
+        html.set(true)
+    }
 }
