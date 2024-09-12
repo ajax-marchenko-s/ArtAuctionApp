ad2d2c13605c887be85f1250696b956bce7951d5
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-09-12 14:59:06 +0300

    feat: add new env properties

---
+++ start-minikube.sh
@@ -0,0 +1,22 @@
+eval $(minikube docker-env)
+
+minikube addons enable ingress
+
+echo "Building project..."
+./gradlew clean
+./gradlew build
+
+echo "Building Docker image..."
+docker build -t artauction-app .
+
+echo "Applying Kubernetes configurations..."
+kubectl apply -f k8s-configuration/mongo-secret.yml
+kubectl apply -f k8s-configuration/mongodb-deployment.yml
+
+kubectl wait --for=condition=ready pod -l app=mongodb --timeout=200s
+
+kubectl apply -f k8s-configuration/artauction-configmap.yml
+kubectl apply -f k8s-configuration/artauction-deployment.yml
+kubectl apply -f k8s-configuration/ingress.yml
+
+kubectl wait --for=condition=ready pod --all --timeout=200s
