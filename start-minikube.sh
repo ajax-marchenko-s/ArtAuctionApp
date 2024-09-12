689fcfb879719fc699108be5851a241427191dd0
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-09-12 15:45:11 +0300

    feat: add mongo-express

--- start-minikube.sh
+++ start-minikube.sh
@@ -10,12 +10,14 @@ docker build -t artauction-app .
 
 echo "Applying Kubernetes configurations..."
 kubectl apply -f k8s-configuration/mongo-secret.yml
+kubectl apply -f k8s-configuration/mongo-configmap.yml
 kubectl apply -f k8s-configuration/mongodb-deployment.yml
 
 kubectl wait --for=condition=ready pod -l app=mongodb --timeout=200s
 
 kubectl apply -f k8s-configuration/artauction-configmap.yml
 kubectl apply -f k8s-configuration/artauction-deployment.yml
+kubectl apply -f k8s-configuration/mongo-express-deployment.yml
 kubectl apply -f k8s-configuration/ingress.yml
 
 kubectl wait --for=condition=ready pod --all --timeout=200s
