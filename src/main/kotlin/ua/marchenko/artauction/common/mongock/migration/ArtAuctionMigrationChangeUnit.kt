0e551fb488e128f0e1e87655cbd9fa4c58f1a3a0
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-09-24 16:55:11 +0300

    fix: add mongock

---
+++ src/main/kotlin/ua/marchenko/artauction/common/mongock/migration/ArtAuctionMigrationChangeUnit.kt
@@ -0,0 +1,35 @@
+package ua.marchenko.artauction.common.mongock.migration
+
+import io.mongock.api.annotations.ChangeUnit
+import io.mongock.api.annotations.Execution
+import io.mongock.api.annotations.RollbackExecution
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Sort
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.index.Index
+import org.springframework.data.mongodb.core.index.IndexOperations
+import ua.marchenko.artauction.user.model.MongoUser
+
+@ChangeUnit(id = "ArtAuctionMigrationChangeUnit", order = "001", author = "svitlana marchenko", systemVersion = "1")
+class ArtAuctionMigrationChangeUnit {
+
+    @Execution
+    fun createIndex(mongoTemplate: MongoTemplate) {
+        val indexOps: IndexOperations = mongoTemplate.indexOps(MongoUser.COLLECTION)
+        indexOps.ensureIndex(
+            Index().on(MongoUser::email.name, Sort.Direction.ASC).unique()
+        )
+        log.info("Index for {} collection for {} field was created", MongoUser.COLLECTION, MongoUser::email.name)
+    }
+
+    @RollbackExecution
+    fun rollbackIndex(mongoTemplate: MongoTemplate) {
+        val indexOps: IndexOperations = mongoTemplate.indexOps(MongoUser.COLLECTION)
+        indexOps.dropIndex("email_1")
+        log.info("Index for {} collection for {} field was rolled back", MongoUser.COLLECTION, MongoUser::email.name)
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(ArtAuctionMigrationChangeUnit::class.java)
+    }
+}
---
+++ src/main/kotlin/ua/marchenko/artauction/common/mongodb/config/MongoConfiguration.kt
@@ -0,0 +1,20 @@
+package ua.marchenko.artauction.common.mongodb.config
+
+import io.mongock.runner.springboot.EnableMongock
+import org.springframework.boot.autoconfigure.domain.EntityScanner
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.data.mongodb.MongoManagedTypes
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Configuration
+@EnableMongock
+class MongoConfiguration {
+    @Bean
+    @Primary
+    fun mongoManagedTypes(applicationContext: ApplicationContext): MongoManagedTypes {
+        return MongoManagedTypes.fromIterable(EntityScanner(applicationContext).scan(Document::class.java))
+    }
+}
