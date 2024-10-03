6c97aa11ae271d9d52fc450b8e06790e263cbe47
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-10-03 09:03:52 +0300

    fix: change to stateful

--- start-minikube.sh
+++ start-minikube.sh
@@ -13,7 +13,7 @@ kubectl apply -f k8s-configuration/mongo-secret.yml
 kubectl apply -f k8s-configuration/mongo-configmap.yml
 kubectl apply -f k8s-configuration/mongo-volume.yml
 kubectl apply -f k8s-configuration/mongo-volume-claims.yml
-kubectl apply -f k8s-configuration/mongodb-deployment.yml
+kubectl apply -f k8s-configuration/mongodb-stateful.yml
 
 kubectl wait --for=condition=ready pod -l app=mongodb --timeout=200s
 
