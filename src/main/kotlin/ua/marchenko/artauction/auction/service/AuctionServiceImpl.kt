f0f21c78bca987daf46bfc4b523aaa6913b6b19b
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-10-04 14:32:28 +0300

    feat: rewrite auction to reactor

--- src/main/kotlin/ua/marchenko/artauction/auction/service/AuctionServiceImpl.kt
+++ src/main/kotlin/ua/marchenko/artauction/auction/service/AuctionServiceImpl.kt
@@ -1,15 +1,15 @@
 package ua.marchenko.artauction.auction.service
 
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import ua.marchenko.artauction.artwork.enums.ArtworkStatus
 import ua.marchenko.artauction.auction.controller.dto.CreateAuctionRequest
-import ua.marchenko.artauction.auction.controller.dto.AuctionResponse
 import ua.marchenko.artauction.auction.repository.AuctionRepository
 import ua.marchenko.artauction.artwork.service.ArtworkService
 import ua.marchenko.artauction.auction.exception.AuctionNotFoundException
 import ua.marchenko.artauction.auction.exception.InvalidAuctionOperationException
 import ua.marchenko.artauction.auction.mapper.toMongo
-import ua.marchenko.artauction.auction.mapper.toResponse
 import ua.marchenko.artauction.auction.model.MongoAuction
 import ua.marchenko.artauction.auction.model.projection.AuctionFull
 
@@ -19,20 +19,24 @@ class AuctionServiceImpl(
     private val artworkService: ArtworkService,
 ) : AuctionService {
 
-    override fun getAll(page: Int, limit: Int): List<MongoAuction> = auctionRepository.findAll(page, limit)
+    override fun getAll(page: Int, limit: Int): Flux<MongoAuction> = auctionRepository.findAll(page, limit)
 
-    override fun getFullAll(page: Int, limit: Int): List<AuctionFull> = auctionRepository.findFullAll(page, limit)
+    override fun getFullAll(page: Int, limit: Int): Flux<AuctionFull> = auctionRepository.findFullAll(page, limit)
 
-    override fun getById(id: String) = auctionRepository.findById(id) ?: throw AuctionNotFoundException(id)
+    override fun getById(id: String): Mono<MongoAuction> =
+        auctionRepository.findById(id).switchIfEmpty(Mono.error(AuctionNotFoundException(id)))
 
-    override fun getFullById(id: String) = auctionRepository.findFullById(id) ?: throw AuctionNotFoundException(id)
+    override fun getFullById(id: String): Mono<AuctionFull> =
+        auctionRepository.findFullById(id).switchIfEmpty(Mono.error(AuctionNotFoundException(id)))
 
-    override fun save(auction: CreateAuctionRequest): AuctionResponse {
-        val artwork = artworkService.getById(auction.artworkId)
-        if (artwork.status != ArtworkStatus.VIEW) {
-            throw InvalidAuctionOperationException("Trying to create auction with non-VIEW artwork")
+    override fun save(auction: CreateAuctionRequest): Mono<MongoAuction> {
+        return artworkService.getById(auction.artworkId).flatMap { artwork ->
+            if (artwork.status != ArtworkStatus.VIEW) {
+                Mono.error(InvalidAuctionOperationException("Trying to create auction with non-VIEW artwork"))
+            } else {
+                artworkService.updateStatus(auction.artworkId, ArtworkStatus.ON_AUCTION)
+                    .then(auctionRepository.save(auction.toMongo()))
+            }
         }
-        artworkService.updateStatus(auction.artworkId, ArtworkStatus.ON_AUCTION)
-        return auctionRepository.save(auction.toMongo()).toResponse()
     }
 }
