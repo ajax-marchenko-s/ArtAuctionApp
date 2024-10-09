9e0bb96b488a1aa95c4bf3a797ef62b121a5d979
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-10-09 15:15:39 +0300

    fix: version in build

--- src/test/kotlin/ua/marchenko/artauction/common/AbstractBaseIntegrationTest.kt
+++ src/test/kotlin/ua/marchenko/artauction/common/AbstractBaseIntegrationTest.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.containers.MongoDBContainer
@@ -12,20 +11,16 @@ import org.testcontainers.containers.MongoDBContainer
 @SpringBootTest
 @ActiveProfiles("test")
 @ContextConfiguration(initializers = [AbstractBaseIntegrationTest.TestContainerInitializer::class])
-@EnableWebFluxSecurity
 interface AbstractBaseIntegrationTest {
 
     companion object {
-        val mongoDBContainer = MongoDBContainer("mongo:latest")
+        val mongoDBContainer = MongoDBContainer("mongo:7.0.14")
             .apply { start() }
     }
 
-    class TestContainerInitializer: ApplicationContextInitializer<ConfigurableApplicationContext> {
+    class TestContainerInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
         override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
-            mongoDBContainer.start()
-            val testPropertyValues = TestPropertyValues.of(
-                "spring.data.mongodb.uri=${mongoDBContainer.replicaSetUrl}"
-            )
+            val testPropertyValues = TestPropertyValues.of("spring.data.mongodb.uri=${mongoDBContainer.replicaSetUrl}")
             testPropertyValues.applyTo(configurableApplicationContext.environment)
         }
     }
