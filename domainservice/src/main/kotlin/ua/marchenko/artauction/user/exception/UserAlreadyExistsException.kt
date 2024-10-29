75463cca668b7fdbd7bdaf87411f4253c7b8e763
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-10-29 11:51:14 +0200

    fix: change UserAlreadyExistsException message

--- domainservice/src/main/kotlin/ua/marchenko/artauction/user/exception/UserAlreadyExistsException.kt
+++ domainservice/src/main/kotlin/ua/marchenko/artauction/user/exception/UserAlreadyExistsException.kt
@@ -2,5 +2,4 @@ package ua.marchenko.artauction.user.exception
 
 import ua.marchenko.artauction.common.exception.type.general.AlreadyExistException
 
-class UserAlreadyExistsException(private val field: String = "email", private val value: String) :
-    AlreadyExistException("User with $field $value already exists")
+class UserAlreadyExistsException : AlreadyExistException("User with the specified attributes already exists")
