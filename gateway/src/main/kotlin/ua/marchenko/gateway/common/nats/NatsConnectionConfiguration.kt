9180dfba577811edad87710204d152b494e85482
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-11-04 10:13:35 +0200

    fix: add var for nats connection to gateway

--- gateway/src/main/kotlin/ua/marchenko/gateway/common/nats/NatsConnectionConfiguration.kt
+++ gateway/src/main/kotlin/ua/marchenko/gateway/common/nats/NatsConnectionConfiguration.kt
@@ -2,11 +2,12 @@ package ua.marchenko.gateway.common.nats
 
 import io.nats.client.Connection
 import io.nats.client.Nats
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class NatsConnectionConfiguration {
+class NatsConnectionConfiguration(@Value("\${nats.uri}") val natsUri: String) {
     @Bean
-    fun connection(): Connection = Nats.connect()
+    fun connection(): Connection = Nats.connect(natsUri)
 }
