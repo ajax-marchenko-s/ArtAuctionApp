09c389f6af0acd859acc2ed5f299a9d6f2f21b8a
Svitlana Marchenko <marchenko.s@ajax.systems>
2024-11-21 11:23:22 +0200

    fix: test change

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,11 @@ allprojects {
         mavenCentral()
 
         maven {
-            url = uri(extra["repository"].toString())
             credentials(AwsCredentials::class.java) {
                 accessKey = extra["AWS_ACCESS_KEY_ID"].toString()
                 secretKey = extra["AWS_SECRET_ACCESS_KEY"].toString()
             }
+            url = uri(extra["repository"].toString())
         }
     }
 }
